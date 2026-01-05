53512843c583806eb947bd8578aa3e76de2287ff
Garieldas <dainiusrainys99@gmail.com>
2026-01-05 11:43:11 +0200

    -project setup

---
+++ send_data.py
@@ -0,0 +1,53 @@
+import os
+from dotenv import load_dotenv
+import requests
+import random
+import time
+from dataclasses import dataclass
+import json
+
+load_dotenv()
+
+try:
+    n8n_url = os.getenv("N8N_WEBHOOK_URL")
+except ValueError:
+    raise ValueError("In case of fuckup...")
+
+@dataclass
+class Payload:
+    """
+    Data payload for sensor readings.
+    param temperature: Temperature reading from the sensor.
+    param humidity: Humidity reading from the sensor.
+    param device: Identifier for the device sending the data.
+    """
+    temperature: int
+    humidity: int
+    device: str = "PC-SENSOR-01"
+
+def generate_payload()-> Payload:
+    """
+    Generate a random payload for sensor data.
+    return: Payload object with random temperature and humidity values.
+    """
+    temperature = random.randint(20, 40)
+    humidity = random.randint(30, 80)
+    return Payload(temperature, humidity)
+
+def send_payload(payload: Payload)-> None:
+    """
+    Send the payload to the n8n webhook URL.
+    param payload: Payload object containing sensor data.
+    return: None
+    """
+    try:
+        response = requests.post(n8n_url, json=json.dumps(payload.__dict__))
+        response.raise_for_status()
+        print(f"Payload sent successfully: {payload}")
+    except requests.RequestException as e:
+        print(f"Failed to send payload: {e}")
+
+while True:
+    payload = generate_payload()
+    send_payload(payload)
+    time.sleep(10)
